fb152b3f2b6322311b37261ea8cdcd7f61937ef1
cyan <qianfeng@qianfengdeMacBook-Pro-96.local>
2015-03-10 17:27:25 +0800

    Initial Commit

---
+++ 爱限免-框架/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  爱限免-框架
+//
+//  Created by cyan on 15-3-10.
+//  Copyright (c) 2015年 cyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 爱限免-框架/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  爱限免-框架
+//
+//  Created by cyan on 15-3-10.
+//  Copyright (c) 2015年 cyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
